65a9637a788e4b7a887378d4aacbb4180fdd4ac4
Minenhle Ncapai <minipai64@gmail.com>
2024-11-23 00:34:55 +0200

    Add fetch logic with basic error handling

--- DJS10/src/App.jsx
+++ DJS10/src/App.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+   fetch( 'https://jsonplaceholder.typicode.com/posts')
+   .then(response => {
+    if (!response.ok) {
+      throw new Error('');
+    
+    return response.json();
+  })
+  .then(data => setPosts(data))
+  .catch(error => setError(error.message));
+  }, []);
+
   return (
     <div>
       <h1>Blog Posts</h1>
