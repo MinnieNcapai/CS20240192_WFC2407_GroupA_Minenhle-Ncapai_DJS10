5a8930219267cc3b4b94799ae157e08b430d5424
Minenhle Ncapai <minipai64@gmail.com>
2024-11-23 00:46:49 +0200

    Display fetched posts

--- DJS10/src/App.jsx
+++ DJS10/src/App.jsx
@@ -19,6 +19,14 @@ function App() {
   return (
     <div>
       <h1>Blog Posts</h1>
+      <ul>
+      {posts.map(post => (
+        <li key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
     </div>
   );
 }
